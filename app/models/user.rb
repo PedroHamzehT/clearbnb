8f7dc1b39f55c17101d4e5af4977f1ed1536d3d8
PedroHamzehT <pedrohamzeh@gmail.com>
2022-04-04 12:57:55 -0300

    setup google auth with devise oauth2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable,
+         :omniauthable, omniauth_providers: [:google]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+
+    user.skip_confirmation!
+    user
+  end
 end
